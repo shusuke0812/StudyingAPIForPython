07342d18ca1108fd801be31a09183ca6cb04bb77
shusuke0812 <shu.ota0812@gmail.com>
2020-08-10 00:05:45 +0900

    [add]リソースファイル追加

---
+++ fastapi/todo_apiv1/task.py
@@ -0,0 +1,14 @@
+tasks = [
+            {
+                'id': 1,
+                'title': 'Buy Beer',
+                'description': 'IPA 6 bottles',
+                'done': False
+            },
+            {
+                'id': 2,
+                'title': 'Buy groceries',
+                'description': 'Beef, Tofu, Sting Onion',
+                'done': False
+            }
+        ]
