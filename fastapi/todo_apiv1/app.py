f36d475cc31bc7988fd4b5183a9085eea7ca0f90
shusuke0812 <shu.ota0812@gmail.com>
2020-08-10 00:09:46 +0900

    [add]タスク一覧をGETするコードを追加

--- fastapi/todo_apiv1/app.py
+++ fastapi/todo_apiv1/app.py
@@ -1,7 +1,14 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from tasks import tasks
 
 app = FastAPI()
 
+'''api動作確認用
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+'''
+
+@app.get("/todo/api/v1.0/tasks")
+def get_tasklist():
+    return {"data": tasks}
