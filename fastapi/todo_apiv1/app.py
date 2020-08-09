ca2d55a9ae9de5cbd6479f0f9e8e7ff3a8a9e320
shusuke0812 <shu.ota0812@gmail.com>
2020-08-10 00:14:23 +0900

    [add]タスクIDを指定してデータを取得する

--- fastapi/todo_apiv1/app.py
+++ fastapi/todo_apiv1/app.py
@@ -9,6 +9,17 @@ def read_root():
     return {"Hello": "World"}
 '''
 
+# タスク一覧を取得
 @app.get("/todo/api/v1.0/tasks")
 def get_tasklist():
     return {"data": tasks}
+
+# タスクIDを指定して取得
+@app.get("/todo/api/v1.0/tasks/{id}")
+def get_task(id):
+    task_id = int(id)
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        raise HTTPException(status_code=404, detail="Not found")
+    else:
+        return {"data": task[0]}
